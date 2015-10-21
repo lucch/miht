c06c443fff6598479f61f6b2bd284c2be6e64fcd
Alexandre Lucchesi <alexandrelucchesi@gmail.com>
2015-10-21 17:03:50 -0200

    Start implementing MIHT_Insert.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+#include "miht.h"
+
 int main()
 {
-	printf("MIHT.\n");
+	struct miht *miht = miht_create(4, 4); 
+
+	struct ip_prefix p = (struct ip_prefix){
+		.prefix   = 1,
+		.suffix   = 0,
+		.len      = 5,
+		.next_hop = 'A'
+	};
+
+	miht_insert(miht, miht->root0, miht->root1, p);
 
 	return 0;
 }
---
+++ src/miht.h
@@ -0,0 +1,50 @@
+#ifndef MIHT_H
+#define MIHT_H
+
+#include <stdbool.h>
+
+/* TEMPORARY TEST STRUCTURE. */
+struct ip_prefix {
+	int prefix;
+	int suffix;
+	int len;
+	char next_hop;
+};
+
+struct ptrie_node {
+	bool is_priority;
+	int suffix;
+	int next_hop;
+	struct ptrie_node *left;
+	struct ptrie_node *right;
+};
+
+struct bplus_node {
+	bool is_leaf;
+	int num_indices;  /* t(u) */
+	int *indices;  /* 1 <= i <= m - 1 */
+	union {
+		struct bplus_node **children;  /* 0 <= i <= m - 1 */
+		struct ptrie_node **data;  /* 1 <= i <= m - 1 */
+	};
+};
+
+struct miht {
+	int k;
+	int m;
+	struct ptrie_node *root0;
+	struct bplus_node *root1;
+};
+
+/*
+ * \brief Recommended value for both \c k and \c m is 16 for IPv4.
+ * \param k Length of prefix keys.
+ * \param m Order of B+ tree.
+ */
+struct miht *miht_create(int k, int m);
+
+void miht_insert(struct miht *miht, struct ptrie_node *ptminusone,
+		struct bplus_node *bplus, struct ip_prefix prefix);
+
+#endif
+
